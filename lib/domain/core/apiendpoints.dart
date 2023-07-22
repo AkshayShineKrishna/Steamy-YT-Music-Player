69d54093022d98afd69d9bd260c9b222355ec983
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-07-22 11:53:51 +0530

    added new endpoints

--- lib/domain/core/apiendpoints.dart
+++ lib/domain/core/apiendpoints.dart
@@ -1,7 +1,8 @@
 import 'package:steamy/core/baseUrl.dart';
 
 class ApiEndpoints {
-  static const streamUrl = '$baseUrl/stream?video_url';
-  static const deleteUrl = '$baseUrl/delete';
-  static const downloadUrl = '$baseUrl/download';
+  static const stream = '$baseUrl/stream?video_url';
+  static const refresh = '$baseUrl/refresh';
+  static const getAudio = '$baseUrl/get_audio';
+  static const download = '$baseUrl/send?video_url';
 }
