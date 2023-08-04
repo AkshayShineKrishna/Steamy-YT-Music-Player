795d81441dc5c0be208f63f5009ed3463a5101c1
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-05 00:41:48 +0530

    added drop shadow

--- lib/presentation/home/widgets/defaultimagewidget.dart
+++ lib/presentation/home/widgets/defaultimagewidget.dart
@@ -37,63 +37,80 @@ class DefaultImageWidget extends StatelessWidget {
                 child: Image.asset('assets/error.png'),
               );
             } else if (state.currentArt != null) {
-              return CachedNetworkImage(
-                imageUrl: state.currentArt!,
-                imageBuilder: (context, imageProvider) => Container(
-                  width: size.width * 0.9,
-                  height: size.width * 0.5,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    image: DecorationImage(
-                      image: imageProvider,
-                      fit: BoxFit.cover,
+              return Container(
+                width: size.width * 0.9,
+                height: size.width * 0.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.4), // Shadow color
+                      spreadRadius: 2, // Spread radius
+                      blurRadius: 10, // Blur radius
+                      offset: Offset(0, 3), // Shadow offset
                     ),
-                  ),
+                  ],
                 ),
-                fit: BoxFit.cover,
-                placeholder: (context, url) => Shimmer.fromColors(
-                  direction:
-                      ShimmerDirection.ltr, // Set the direction of the shimmer
-                  baseColor: kdeepPurpleHighlight,
-                  highlightColor: Colors.deepPurple,
-                  child: Container(
+                child: CachedNetworkImage(
+                  imageUrl: state.currentArt!,
+                  imageBuilder: (context, imageProvider) => Container(
                     width: size.width * 0.9,
                     height: size.width * 0.5,
                     decoration: BoxDecoration(
                       borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      color: Colors.grey[
-                          300], // Placeholder color while the image is loading
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                errorWidget: (context, url, error) => Container(
-                  width: size.width * 0.9,
-                  height: size.width * 0.5,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    color: kdeepPurpleHighlight, // Placeholder color in case of an error
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => Shimmer.fromColors(
+                    direction: ShimmerDirection
+                        .ltr, // Set the direction of the shimmer
+                    baseColor: Colors.purple[900]!,
+                    highlightColor: Colors.deepPurple,
+                    child: Container(
+                      width: size.width * 0.9,
+                      height: size.width * 0.5,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        color: Colors.grey[
+                            300], // Placeholder color while the image is loading
+                      ),
+                    ),
                   ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.error,
-                        size: 50,
-                        color: kWhiteFont,
-                      ),
-                      kHeight,
-                      Text(
-                        'Error Loading Thumbnail !',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w700),
-                      ),
-                      Text('Try Reloading 🥲',
+                  errorWidget: (context, url, error) => Container(
+                    width: size.width * 0.9,
+                    height: size.width * 0.5,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      color:
+                          kdeepPurpleHighlight, // Placeholder color in case of an error
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.error,
+                          size: 50,
+                          color: kWhiteFont,
+                        ),
+                        kHeight,
+                        Text(
+                          'Error Loading Thumbnail !',
                           style: TextStyle(
-                              color: kWhiteFont,
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ), // Widget to display in case of an error
-                ), // Set the fit for the image
+                              color: Colors.white, fontWeight: FontWeight.w700),
+                        ),
+                        Text('Try Reloading 🥲',
+                            style: TextStyle(
+                                color: kWhiteFont,
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ), // Widget to display in case of an error
+                  ), // Set the fit for the image
+                ),
               );
             }
             return SizedBox(
