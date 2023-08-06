e15b1c35b970fc8fa71b025c9cda16f1026b8e0c
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-06 19:43:08 +0530

    added const width

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -10,3 +10,4 @@ const kHeight = SizedBox(height: 15);
 const kHeightMedium = SizedBox(height: 20);
 const kHeightLarge = SizedBox(height: 40);
 const kHeightXL = SizedBox(height: 100);
+const kWidth = SizedBox(width: 15);
