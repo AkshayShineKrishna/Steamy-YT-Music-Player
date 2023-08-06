0f62b32074cc0bcd8d3571c21953d6a224f20510
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-06 19:42:55 +0530

    fixed imports

--- lib/application/menu/menu_bloc.dart
+++ lib/application/menu/menu_bloc.dart
@@ -3,7 +3,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:steamy/domain/menu_item/menu_data.dart';
 import 'package:steamy/domain/menu_item/menu_item.dart';
-
 part 'menu_event.dart';
 part 'menu_state.dart';
 part 'menu_bloc.freezed.dart';
--- lib/core/di/injectable.config.dart
+++ lib/core/di/injectable.config.dart
@@ -11,12 +11,13 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:steamy/application/home/home_bloc.dart' as _i8;
+import 'package:steamy/application/home/home_bloc.dart' as _i9;
 import 'package:steamy/application/menu/menu_bloc.dart' as _i5;
+import 'package:steamy/application/playlist/playlist_bloc.dart' as _i6;
 import 'package:steamy/domain/get_audio/get_audio_services.dart' as _i3;
-import 'package:steamy/domain/refresh/refreshservices.dart' as _i6;
+import 'package:steamy/domain/refresh/refreshservices.dart' as _i7;
 import 'package:steamy/infrastructure/get_audio_impl.dart' as _i4;
-import 'package:steamy/infrastructure/refresh_impl.dart' as _i7;
+import 'package:steamy/infrastructure/refresh_impl.dart' as _i8;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -31,10 +32,11 @@ extension GetItInjectableX on _i1.GetIt {
     );
     gh.lazySingleton<_i3.GetAudioServices>(() => _i4.GetAudioImpl());
     gh.factory<_i5.MenuBloc>(() => _i5.MenuBloc());
-    gh.lazySingleton<_i6.RefreshServices>(() => _i7.RefreshImpl());
-    gh.factory<_i8.HomeBloc>(() => _i8.HomeBloc(
+    gh.factory<_i6.PlaylistBloc>(() => _i6.PlaylistBloc());
+    gh.lazySingleton<_i7.RefreshServices>(() => _i8.RefreshImpl());
+    gh.factory<_i9.HomeBloc>(() => _i9.HomeBloc(
           gh<_i3.GetAudioServices>(),
-          gh<_i6.RefreshServices>(),
+          gh<_i7.RefreshServices>(),
         ));
     return this;
   }
