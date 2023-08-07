4fd2e7d736d871099821726275382287fdd5913d
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-07 20:31:39 +0530

    added category model

---
+++ lib/domain/category/category.dart
@@ -0,0 +1,21 @@
+class Category {
+  static const List<String> categoryList = [
+    "😎 Mood",
+    "🎧 Focus",
+    "💖 Romantic",
+    "🎉 Party",
+    "🌙 Chill",
+    "🚗 Road Trip",
+    "🏋️‍♀️ Workout",
+    "🧡 Feel-Good",
+    "📚 Study",
+    "🎼 Lofi",
+    "🎸 Rock",
+    "🎤 Pop",
+    "🕺 Dance",
+    "🎵 Acoustic",
+    "💤 Sleep",
+    "🎶 Classics",
+    "🌨️ Rainy Day"
+  ];
+}
