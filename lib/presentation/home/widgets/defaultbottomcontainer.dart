35db6965a300e3e3f8fb3a40769cf7ed639801ab
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-11 13:29:31 +0530

    added like button

--- lib/presentation/home/widgets/defaultbottomcontainer.dart
+++ lib/presentation/home/widgets/defaultbottomcontainer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:like_button/like_button.dart';
+import 'package:steamy/application/home/home_bloc.dart';
 import 'package:steamy/core/constants.dart';
 
 class DefaultBottomContainer extends StatelessWidget {
@@ -13,16 +16,32 @@ class DefaultBottomContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-        width: double.infinity,
-        height: size.height < 800 ? size.height * 0.35 : size.height * 0.4,
-        decoration: const BoxDecoration(
-          color: kWhite,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(50),
-            topRight: Radius.circular(50),
-          ),
-        ),
-        child: widget);
+    return Stack(
+      children: [
+        Container(
+            width: double.infinity,
+            height: size.height < 800 ? size.height * 0.35 : size.height * 0.4,
+            decoration: const BoxDecoration(
+              color: kWhite,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+            ),
+            child: widget),
+        BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, state) {
+            if (state.responseResult.isNotEmpty) {
+              return const Positioned(
+                top: 45,
+                right: 20,
+                child: LikeButton(),
+              );
+            }
+            return kHeight;
+          },
+        )
+      ],
+    );
   }
 }
