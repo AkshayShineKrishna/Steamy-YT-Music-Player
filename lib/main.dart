bbdb7fecb9d32eded50be52974fbf9d1c98212ec
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-17 17:34:46 +0530

    Added Hive Adapters

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:steamy/application/home/home_bloc.dart';
 import 'package:steamy/application/menu/menu_bloc.dart';
 import 'package:steamy/application/playlist/playlist_bloc.dart';
+import 'package:steamy/domain/playlist/model/playlist_data.dart';
+import 'package:steamy/domain/playlist/model/song_data.dart';
 import 'package:steamy/presentation/splash/splash.dart';
 import 'core/di/injectable.dart';
 
@@ -16,6 +20,24 @@ Future<void> main() async {
     androidNotificationChannelName: 'Audio playback',
     androidNotificationOngoing: true,
   );
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(PlaylistAdapter().typeId)) {
+    Hive.registerAdapter(PlaylistAdapter());
+  }
+  if (!Hive.isAdapterRegistered(SongDataAdapter().typeId)) {
+    Hive.registerAdapter(SongDataAdapter());
+  }
+  // Open the 'playlists' box
+  await Hive.openBox<Playlist>('playlists');
+  await Hive.openBox<SongData>('songs');
+
+  // Check if 'Liked' playlist exists, and create it if not
+  final likedPlaylist = Hive.box<Playlist>('playlists').get('Liked');
+  log('value of likedPlaylist : ${likedPlaylist.toString()}');
+  if (likedPlaylist == null) {
+    final newLikedPlaylist = Playlist(name: 'Liked', songs: []);
+    Hive.box<Playlist>('playlists').put('Liked', newLikedPlaylist);
+  }
   runApp(const MyApp());
 }
 
