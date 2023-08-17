35fc322ecb5c09c5905e4ed6329394e6cb11be6a
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-17 17:41:35 +0530

    build runner file

--- lib/domain/playlist/model/playlist_data.g.dart
+++ lib/domain/playlist/model/playlist_data.g.dart
@@ -6,27 +6,27 @@ part of 'playlist_data.dart';
 // TypeAdapterGenerator
 // **************************************************************************
 
-class PlaylistDataAdapter extends TypeAdapter<PlaylistData> {
+class PlaylistAdapter extends TypeAdapter<Playlist> {
   @override
   final int typeId = 0;
 
   @override
-  PlaylistData read(BinaryReader reader) {
+  Playlist read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return PlaylistData(
-      fields[0] as String,
-      (fields[1] as List).cast<SongData>(),
-      fields[2] as String?,
-      fields[3] as String,
-      fields[4] as String?,
+    return Playlist(
+      name: fields[0] as String,
+      songs: (fields[1] as List).cast<SongData>(),
+      mood: fields[2] as String?,
+      duration: fields[3] as String,
+      description: fields[4] as String?,
     );
   }
 
   @override
-  void write(BinaryWriter writer, PlaylistData obj) {
+  void write(BinaryWriter writer, Playlist obj) {
     writer
       ..writeByte(5)
       ..writeByte(0)
@@ -47,7 +47,7 @@ class PlaylistDataAdapter extends TypeAdapter<PlaylistData> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is PlaylistDataAdapter &&
+      other is PlaylistAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
