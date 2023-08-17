efe46828466240887f1756c5aa40c76b8b58c7c0
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-08-17 17:37:56 +0530

    formated code

--- lib/domain/get_audio/get_audio_services.dart
+++ lib/domain/get_audio/get_audio_services.dart
@@ -2,7 +2,6 @@ import 'package:steamy/domain/core/failure/main_failure.dart';
 import 'package:steamy/domain/get_audio/Model/get_audio_response.dart';
 import 'package:dartz/dartz.dart';
 
-
 abstract class GetAudioServices {
   Future<Either<MainFailure, GetAudioResponse>> getAudio({
     required String youtubeUrl,
