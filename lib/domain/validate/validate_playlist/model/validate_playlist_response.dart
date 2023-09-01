9816488844ba0f115b72dcf0f7fdfd911339b3f5
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-09-01 14:21:21 +0530

    updated response

--- lib/domain/validate/validate_playlist/model/validate_playlist_response.dart
+++ lib/domain/validate/validate_playlist/model/validate_playlist_response.dart
@@ -3,28 +3,14 @@ part 'validate_playlist_response.g.dart';
 
 @JsonSerializable()
 class ValidatePlaylistResponse {
-  @JsonKey(name: 'result')
-  List<PlalistResultResponse>? result;
+  @JsonKey(name: 'duration')
+  String? duration;
 
-  ValidatePlaylistResponse({this.result});
+  ValidatePlaylistResponse({this.duration});
 
   factory ValidatePlaylistResponse.fromJson(Map<String, dynamic> json) {
     return _$ValidatePlaylistResponseFromJson(json);
   }
 
   Map<String, dynamic> toJson() => _$ValidatePlaylistResponseToJson(this);
-}
-
-@JsonSerializable()
-class PlalistResultResponse {
-  @JsonKey(name: 'total_duration')
-  String? totalDuration;
-
-  PlalistResultResponse({this.totalDuration});
-
-  factory PlalistResultResponse.fromJson(Map<String, dynamic> json) {
-    return _$PlalistResultResponseFromJson(json);
-  }
-
-  Map<String, dynamic> toJson() => _$PlalistResultResponseToJson(this);
 }
--- lib/domain/validate/validate_playlist/model/validate_playlist_response.g.dart
+++ lib/domain/validate/validate_playlist/model/validate_playlist_response.g.dart
@@ -9,26 +9,11 @@ part of 'validate_playlist_response.dart';
 ValidatePlaylistResponse _$ValidatePlaylistResponseFromJson(
         Map<String, dynamic> json) =>
     ValidatePlaylistResponse(
-      result: (json['result'] as List<dynamic>?)
-          ?.map(
-              (e) => PlalistResultResponse.fromJson(e as Map<String, dynamic>))
-          .toList(),
+      duration: json['duration'] as String?,
     );
 
 Map<String, dynamic> _$ValidatePlaylistResponseToJson(
         ValidatePlaylistResponse instance) =>
     <String, dynamic>{
-      'result': instance.result,
-    };
-
-PlalistResultResponse _$PlalistResultResponseFromJson(
-        Map<String, dynamic> json) =>
-    PlalistResultResponse(
-      totalDuration: json['total_duration'] as String?,
-    );
-
-Map<String, dynamic> _$PlalistResultResponseToJson(
-        PlalistResultResponse instance) =>
-    <String, dynamic>{
-      'total_duration': instance.totalDuration,
+      'duration': instance.duration,
     };
