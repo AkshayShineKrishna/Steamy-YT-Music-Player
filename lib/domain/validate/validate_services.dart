7ea491367e30ec8101c426cf9ee0186196c7ae0a
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-09-01 14:21:05 +0530

    updated parameters

--- lib/domain/validate/validate_services.dart
+++ lib/domain/validate/validate_services.dart
@@ -4,7 +4,7 @@ import 'package:steamy/domain/validate/validate_playlist/model/validate_playlist
 import 'package:steamy/domain/validate/validate_song/model/validate_song_data_response.dart';
 
 abstract class ValidateServices {
-  Future<Either<MainFailure, ValidatePlaylistResponse>> validatePlaylist();
+  Future<Either<MainFailure, ValidatePlaylistResponse>> validatePlaylist({required List<String> urlList});
   Future<Either<MainFailure, ValidateSongDataResponse>> validateSong();
 
 }
